f95955ceef2aa4c653ad0da5e7f625edb241bcc3
Landon Schropp <schroppl@gmail.com>
2024-10-17 21:23:25 -0700

    Add incomplete tasks to the tasks to forward

--- src/constants.ts
+++ src/constants.ts
@@ -7,5 +7,8 @@ export const SCHEDULED_TYPE = "<";
 /** A to-do task. */
 export const TO_DO_TYPE = " ";
 
+/** An incomplete task. */
+export const INCOMPLETE_TYPE = "/";
+
 /** The types of tasks that are actionable by the plugin. */
-export const ACTIONABLE_TASK_TYPES = [FORWARDED_TYPE, SCHEDULED_TYPE];
+export const ACTIONABLE_TASK_TYPES = [FORWARDED_TYPE, SCHEDULED_TYPE, INCOMPLETE_TYPE];
